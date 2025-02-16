2ec9346e9ef53bcadc27e7800cbcdf6a78f5d1e8
Abush Natanem <abushnatanem@gmail.com>
2025-02-16 21:02:40 +0300

    update nodemailer with brevo smtp

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,11 +1,10 @@
 import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: process.env.SENDER_EMAIL,
+    user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
 });
