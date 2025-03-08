aaac780bb13c01bdebbf6b79945d52aacaaa2f7e
Abush Natanem <abushnatanem@gmail.com>
2025-03-08 21:47:24 +0300

    update the context

--- frontend/src/features/context.jsx
+++ frontend/src/features/context.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(true);
   const [userData, setUserData] = useState({});
 
   const signIn = (user) => {
